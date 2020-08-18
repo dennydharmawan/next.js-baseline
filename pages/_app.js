230db732fc85bd5081b205f414ee56f01195a49c
denny dharmawan <denny.dharmawan@btpn.com>
2020-08-18 21:32:46 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,12 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Head from '../components/Head';
-import { Provider } from 'next-auth/client';
 
+import Head from '../components/Head';
 import theme from '../lib/theme';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
-  const { session } = pageProps;
 
   useEffect(() => {
     // Remove the server-side injected CSS.
@@ -21,19 +19,12 @@ export default function MyApp(props) {
 
   return (
     <>
-      <Provider options={{ site: process.env.SITE }} session={session}>
-        <Head></Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </Provider>
+      <Head></Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,3 @@
-import React from 'react';
-import { Button, Container } from '@material-ui/core';
-import { signIn, signOut, useSession } from 'next-auth/client';
-
-export default function Index() {
-  const [session, loading] = useSession();
-
-  return (
-    <>
-      {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={signIn}>Sign in</button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session.user.email} <br />
-          <button onClick={signOut}>Sign out</button>
-        </>
-      )}
-    </>
-  );
+export default function index() {
+  return <div>Hello world</div>;
 }
